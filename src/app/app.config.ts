92f90ed304aa32263fa77379b198596956cdc9b8
Ocunidee <ocunidee@gmail.com>
2025-01-13 18:04:16 +0100

    Angular 19 correction

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
 import { registerLocaleData } from '@angular/common'
-// import localeFr from '@angular/common/locales/fr'
+import localeFr from '@angular/common/locales/fr'
 
-// registerLocaleData(localeFr, 'fr')
+registerLocaleData(localeFr, 'fr')
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
