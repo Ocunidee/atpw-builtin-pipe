9673edaea37d23f86d35972f13bdfd37b3270134
Ocunidee <ocunidee@gmail.com>
2025-08-26 15:39:27 +0200

    Angular 20 - zoneless & signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, computed, signal } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  price = 9.9
-  expirationDate = new Date()
+export class AppComponent {
+  protected readonly price = signal(9.9)
+  protected readonly today = signal(Date.now())
+  protected readonly expirationDate = computed(() => {
+    const date = new Date(this.today())
+    date.setDate(date.getDate() + 3)
 
-  ngOnInit() {
-    this.expirationDate.setDate(this.expirationDate.getDate() + 3)
-  }
+    return date
+  })
 }
