7a886b391716f6610ef224d8822caea39b5b6ce9
Ocunidee <ocunidee@gmail.com>
2025-08-26 15:42:50 +0200

    Angular 20 - zoneless & signals correction

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
-// import { registerLocaleData } from '@angular/common'
-// import localeFr from '@angular/common/locales/fr'
+import { registerLocaleData } from '@angular/common'
+import localeFr from '@angular/common/locales/fr'
 
-// registerLocaleData(localeFr, 'fr')
+registerLocaleData(localeFr, 'fr')
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZonelessChangeDetection(), provideRouter(routes)]
